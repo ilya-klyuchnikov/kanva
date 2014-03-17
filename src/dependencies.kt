aa832f31c0bd53cdd1075d8a9e32d40e7300de2a
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2014-03-17 16:24:07 +0400

    dependencies

--- src/dependencies.kt
+++ src/dependencies.kt
@@ -1,13 +1,33 @@
 package kanva.dependencies
 
+import java.util.ArrayList
+import java.util.Collections
+import java.util.IdentityHashMap
+import java.util.Stack
+import java.util.LinkedList
+import java.util.HashSet
+
+import org.objectweb.asm.ClassReader.*
+import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.Opcodes
+
+import kotlinlib.flags
 import kanva.index.*
 import kanva.graphs.*
 import kanva.declarations.*
-import org.objectweb.asm.ClassReader.*
-import org.objectweb.asm.ClassVisitor
-import org.objectweb.asm.Opcodes
-import org.objectweb.asm.MethodVisitor
-import kotlinlib.flags
+
+fun <T> Graph<T>.getTopologicallySortedStronglyConnectedComponents(): List<Set<Node<T>>> {
+    val sccFinder = SCCFinder(this, { g -> g.nodes }, { m -> m.outgoingEdges.map{ it.to } })
+    val components = sccFinder.getAllComponents()
+
+    return components.topologicallySort {
+        c ->
+        c.map {
+            m -> sccFinder.findComponent(m)
+        }
+    }
+}
 
 public fun buildFunctionDependencyGraph(declarationIndex: DeclarationIndex, classSource: ClassSource) : Graph<Method> =
         FunDependencyGraphBuilder(declarationIndex, classSource).build()
@@ -41,16 +61,128 @@ public class FunDependencyGraphBuilder(
         }
     }
 
-
     override fun newGraph(): GraphImpl<Method> = GraphImpl(false)
     override fun newNode(data: Method): NodeImpl<Method> = DefaultNodeImpl(data)
 
     public fun build(): Graph<Method> {
-        source.forEach {
-            reader ->
-            reader.accept(classVisitor, flags(SKIP_DEBUG, SKIP_FRAMES))
+        source.forEach { it.accept(classVisitor, flags(SKIP_DEBUG, SKIP_FRAMES)) }
+        return toGraph()
+    }
+}
+
+private fun <T: Any> identityHashSet(): MutableSet<T> = Collections.newSetFromMap(IdentityHashMap())
+
+fun <T> Iterable<T>.topologicallySort(adjacentNodes: (T) -> Iterable<T>): List<T> {
+    val result = LinkedList<T>()
+    val visited = HashSet<T>()
+
+    fun processNode(node: T) {
+        if (!visited.add(node)) return
+        for (next in adjacentNodes(node)) {
+            processNode(next)
+        }
+        result.addFirst(node)
+    }
+
+    for (node in this) {
+        processNode(node)
+    }
+
+    return result
+}
+
+private class SCCFinder<Graph, Node>(
+        val graph: Graph,
+        val graphNodes: (Graph) -> Iterable<Node>,
+        val adjacentNodes: (Node) -> Iterable<Node>) {
+
+    private class IdentityHashStack<T> {
+        val stack = Stack<T>()
+        val identitySet = identityHashSet<T>()
+
+        fun contains(elem : T) : Boolean = identitySet.contains(elem)
+        fun push(elem : T) {
+            stack.push(elem)
+            assert(identitySet.add(elem))
+        }
+        fun pop() : T {
+            val elem = stack.pop()!!
+            assert(identitySet.remove(elem))
+            return elem
+        }
+        fun isEmpty() : Boolean {
+            assert(stack.isEmpty() == identitySet.isEmpty())
+            return stack.isEmpty()
+        }
+    }
+
+    private val components = ArrayList<Set<Node>>()
+    private val nodeToComponent = IdentityHashMap<Node, Set<Node>>()
+
+    // Index of visited nodes
+    private val nodeIndex = IdentityHashMap<Node, Int>()
+
+    // Current index for not-visited node
+    private var index = 0
+
+    public fun findComponent(node: Node): Set<Node> {
+        if (!nodeIndex.containsKey(node)) {
+            execute(node)
         }
 
-        return toGraph()
+        return nodeToComponent[node] ?: throw IllegalStateException("Can't find component for node ${node}")
+    }
+
+    public fun getAllComponents(): List<Set<Node>> {
+        for (node in graphNodes(graph)) {
+            findComponent(node)
+        }
+        return components
+    }
+
+    private fun execute(node: Node) {
+        val stack = IdentityHashStack<Node>();
+        val minUnvisitedReachable = IdentityHashMap<Node, Int>()
+
+        executeOnNode(node, stack, minUnvisitedReachable)
+
+        assert(stack.isEmpty())
+    }
+
+    private fun executeOnNode(node: Node, stack: IdentityHashStack<Node>, minUnvisitedReachable: MutableMap<Node, Int>) {
+        val currentNodeIndex = index
+
+        minUnvisitedReachable[node] = currentNodeIndex
+        nodeIndex[node] = currentNodeIndex
+
+        index++
+
+        stack.push(node);
+
+        for (nextNode in adjacentNodes(node)) {
+            val currentComponentIndex = minUnvisitedReachable[node]!!
+
+            if (!nodeIndex.containsKey(nextNode)) {
+                executeOnNode(nextNode, stack, minUnvisitedReachable);
+                minUnvisitedReachable[node] = Math.min(currentComponentIndex, minUnvisitedReachable[nextNode]!!)
+            } else if (stack.contains(nextNode)) {
+                minUnvisitedReachable[node] = Math.min(currentComponentIndex, nodeIndex[nextNode]!!)
+            }
+        }
+
+        if (minUnvisitedReachable[node] == currentNodeIndex){
+            val component = identityHashSet<Node>()
+            do {
+                val componentNode = stack.pop()
+                component.add(componentNode)
+                nodeToComponent[componentNode] = component
+
+                if (componentNode identityEquals node) {
+                    break
+                }
+            } while(true)
+
+            components.add(component)
+        }
     }
 }
